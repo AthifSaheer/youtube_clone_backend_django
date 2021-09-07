8f1ff2db902f483ae927fb019dcaed966a0d9a4d
Athif Saheer <liteboook@gmail.com>
2021-09-07 12:17:49 +0530

    redatabase changed to psql

--- project/settings.py
+++ project/settings.py
@@ -102,13 +102,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'youtubeclone',
+        'USER': 'youtubeclone',
+        'PASSWORD': 'youtubeclone',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
